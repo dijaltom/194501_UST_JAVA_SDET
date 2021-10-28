6f9b39b43913572a8544f4b01df14396f36b0b39
Dijal <dijal.tom@prvak.in>
2021-10-28 12:33:02 +0530

    m3a

--- Protractor/Specs/spec1.js
+++ Protractor/Specs/spec1.js
@@ -1,7 +1,7 @@
 describe('angularjs homepage todo list', function() {
     it('should add a todo', function() {
       browser.get('https://angularjs.org');
-  
+      
       element(by.model('todoList.todoText')).sendKeys('write first protractor test');
       element(by.css('[value="add"]')).click();
   
